265981688bd6dffde1ee70712ede2c2b62645592
Max Home <max@huzm.ru>
2016-06-01 12:22:32 +0300

    First try of IR reciever

--- ir-uni-control.ino
+++ ir-uni-control.ino
@@ -1,10 +1,11 @@
-#include <IRremote.h>
-
 /* ***
 Project: Arduino Universal IR Control
 Author:  Max Huzmiev
 email:   max@huzm.ru
 *** */
+
+#include <IRremote.h>
+#include "ir-codes/TV--SAMSUNG-LE32C630K1W.h"
 
 unsigned char irPin = 4;
 IRrecv irrecv(irPin);
@@ -17,7 +18,11 @@ void setup() {
 
 void loop() {
   if (irrecv.decode(&results)) {
-    Serial.println(results.value, HEX);
+    if (results.value == VOLUME_UP)   Serial.write("Volume up\n");
+    if (results.value == VOLUME_DOWN)   Serial.write("Volume down\n");
+    if (results.value == CHANNEL_UP)   Serial.write("Channel up\n");
+    if (results.value == CHANNEL_DOWN)   Serial.write("Channel down\n");
+    if (results.value == OK)   Serial.write("OK\n");
     irrecv.resume(); // Receive the next value
   }
   delay(100);  
