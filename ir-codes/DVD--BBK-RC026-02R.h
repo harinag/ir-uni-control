387ec6d7f77c8a5a9fe1b1a0cb26e00ca0026bfe
Max Home <max@huzm.ru>
2016-06-02 23:30:09 +0300

    Added codes file for BBK DVD Karaoke player

---
+++ ir-codes/DVD--BBK-RC026-02R.h
@@ -0,0 +1,13 @@
+// Remote Control Codes
+// Device type: DVD player + Karaoke
+// Model: BBK RC026-02R
+
+namespace RC026_02R {
+
+	const long POWER = 0x926D00FF;
+	const long VOLUME_UP = 0x926DC837;
+	const long VOLUME_DOWN = 0x926D48B7;
+	const long OK = 0x926DB24D;
+	const long MENU = 0x926D50AF;
+
+}
--- ir-uni-control.ino
+++ ir-uni-control.ino
@@ -7,17 +7,30 @@ email:   max@huzm.ru
 #include <IRremote.h>
 #include "ir-codes/TV--SAMSUNG-LE32C630K1W.h"
 #include "ir-codes/DVBT2--HYUNDAI-H-DVB01T2.h"
+#include "ir-codes/DVD--BBK-RC026-02R.h"
 
 namespace TV = LE32C630K1W;
 namespace DVB = H_DVB01T2;
+namespace DVD = RC026_02R;
 
 unsigned char irPin = 4;
 IRrecv irrecv(irPin);
+IRsend irsend;
 decode_results results;
 
 void setup() {
   irrecv.enableIRIn();  // Start the receiver
   Serial.begin(9600);
+  pinMode(3, OUTPUT);
+  for (int i=0; i<5; i++) {
+    //irsend.sendNEC(DVB::VOLUME_UP, 32);
+    digitalWrite(3, HIGH);
+    delay(300);
+    digitalWrite(3, LOW);
+    delay(300);
+  }
+  irsend.sendNEC(DVD::VOLUME_UP, 32);
+  Serial.write("DVB signal sent");
 }
 
 void loop() {
@@ -32,6 +45,10 @@ void loop() {
     if (results.value == DVB::CHANNEL_UP)   Serial.write("Channel up (Pristavka)\n");
     if (results.value == DVB::CHANNEL_DOWN)   Serial.write("Channel down (Pristavka)\n");
     if (results.value == DVB::OK)   Serial.write("OK (Pristavka)\n");
+    if (results.value == DVD::VOLUME_UP)   Serial.write("Volume up (DVD Karaoke)\n");
+    if (results.value == DVD::VOLUME_DOWN)   Serial.write("Volume down (DVD Karaoke)\n");
+    if (results.value == DVD::OK)   Serial.write("OK (DVD Karaoke)\n");
+
     irrecv.resume(); // Receive the next value
   }
   delay(100);  
